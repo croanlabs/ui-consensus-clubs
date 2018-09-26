c2c98fd9728978054bc9048e651b17fd0c966588
Declan Arkins <dec.arkins@gmail.com>
2018-09-27 00:48:38 +0100

    adding several components, merits, comgratulations, withdrawModify

---
+++ consensusgame/src/Components/WithdrawModify/WithdrawModify.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import infoIcon from "../../assets/icons/info-icon.png";
+import "./WithdrawModify.scss";
+
+const WithdrawModify = () => {
+  return (
+    <div className="withdraw-modify">
+        <form class="form">
+            <div class="switch-field">
+                <input type="radio" id="switch_left" name="switch_2" value="Withdraw" checked/>
+                <label for="switch_left">Withdraw</label>
+                <input type="radio" id="switch_right" name="switch_2" value="modify" />
+                <label for="switch_right">Modify</label>
+            </div>
+        </form>
+        <div className="container">
+            <p className="current-value">
+                Current Value
+            </p>
+            <p className="merits-count">
+                1,231 Merits
+            </p>
+            <p className="withdraw-support">
+                Withdraw my support for an <span>18.3%</span> gain
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default WithdrawModify;
