900c804b3c161b1c59d1c5d6c3897bf2c593deb9
Jasmineee2 <kikmnr.y@gmail.com>
2018-10-07 22:40:15 +0100

    Add profile menu bar

--- consensusgame/src/Components/Authentication/Authentication.js
+++ consensusgame/src/Components/Authentication/Authentication.js
@@ -1,38 +1,38 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import TwitterLogin from 'react-twitter-auth';
-import Cookies from 'universal-cookie';
-import '../NavBar/NavBar.scss';
-import './Authentication.scss';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import TwitterLogin from "react-twitter-auth";
+import Cookies from "universal-cookie";
+import "../NavBar/NavBar.scss";
+import "./Authentication.scss";
 
 class Authentication extends Component {
   constructor() {
     super();
 
     let cookies = new Cookies();
-    let user = cookies.get('user');
+    let user = cookies.get("user");
     if (user) {
-      this.state = {user};
+      this.state = { user, profileMenuOpen: false };
     } else {
       this.state = {};
     }
   }
 
   onSuccess = async response => {
-    const token = response.headers.get('x-auth-token');
+    const token = response.headers.get("x-auth-token");
     if (token) {
       // Add cookies for token and user info
       let cookies = new Cookies();
-      cookies.set('token', token, {
+      cookies.set("token", token, {
         // FIXME Set httpOnly.
         //httpOnly: true,
-        path: '/',
+        path: "/"
       });
 
       const user = await response.json();
-      cookies.set('user', JSON.stringify(user), {httpOnly: false, path: '/'});
+      cookies.set("user", JSON.stringify(user), { httpOnly: false, path: "/" });
 
-      this.setState({user});
+      this.setState({ user });
     }
   };
 
@@ -45,15 +45,59 @@ class Authentication extends Component {
     cookies;
   };
 
+  handleProfileMenuOpen = () => {
+    this.setState(prevState => {
+      return { profileMenuOpen: !prevState.profileMenuOpen };
+    });
+  };
+
   render() {
+    let ProfileMenu;
+    this.state.profileMenuOpen
+      ? (ProfileMenu = (
+          <ul className="profile-menu">
+            <li>
+              <Link className="nav-link" to="/profile">
+                Leader board
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/">
+                Settings
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/">
+                FAQ
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/">
+                Policy
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/">
+                Terms {"&"} Conditions
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/">
+                Sign out
+              </Link>
+            </li>
+          </ul>
+        ))
+      : null;
+
     let content = this.state.user ? (
-      <div>
-        <Link className="nav-link" to="/profile">
-          <div class="profile-pic">
-            <img src={this.state.user.profileImageUrl} alt="Profile" />
-          </div>
-        </Link>
-      </div>
+      <React.Fragment>
+        <div class="profile-pic" onClick={this.handleProfileMenuOpen}>
+          <img src={this.state.user.profileImageUrl} alt="Profile" />
+        </div>
+        <div className="user-total-merits">1000</div>
+        {ProfileMenu}
+      </React.Fragment>
     ) : (
       <TwitterLogin
         loginUrl={
